29b2faca700632cacfc00f0498f6d960a675e9ea
Enjeelachaudhary <cenjeela12@gmail.com>
2021-01-17 21:15:28 +0545

    view

--- app/src/main/java/com/example/weeksixassignmentone/Adapter/ViewPageAdapter.kt
+++ app/src/main/java/com/example/weeksixassignmentone/Adapter/ViewPageAdapter.kt
@@ -4,7 +4,7 @@ package com.example.weeksixassignmentone.Adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-
+//view
 class ViewPageAdapter(
     supportFragmentManager: FragmentManager
 )  : FragmentPagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
